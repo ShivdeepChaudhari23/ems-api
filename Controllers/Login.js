7259a7157cdb38bf9aa96d28f561ace80c54131d
ShivdeepChaudhari23 <shivdeepchaudhari23@gmail.com>
2024-07-17 10:19:18 +0530

    Session Validation API

--- Controllers/Login.js
+++ Controllers/Login.js
@@ -44,4 +44,47 @@ const adminLogin = async (req, res) => {
     }
 };
 
-export { adminLogin };
+const verifyToken = async (req, res) => {
+    try {
+        const authorizationHeader = req.headers.authorization;
+        const authToken = authorizationHeader.split(' ')[1];
+        if (!authToken) {
+            res.status(400);
+            return res.json({
+                error: 'Token Not Found',
+                isSessionValid: false,
+            })
+        }
+
+        const authData = jwt.decode(authToken, { complete: true });
+
+        if (!authData) {
+            res.status(400);
+            return res.json({
+                error: 'Invalid Token',
+                isSessionValid: false,
+            });
+        }
+
+        const tokenExpiry = authData.payload.exp;
+
+        if ((tokenExpiry * 1000) < Date.now()) {
+            res.status(401);
+            return res.json({
+                error: 'Token Expired',
+                isSessionValid: false,
+            });
+        }
+
+        return res.json({
+            isSessionValid: true,
+        })
+    } catch (e) {
+        res.status(500);
+        return res.json({
+            error: 'Something went wrong',
+        })
+    }
+}
+
+export { adminLogin, verifyToken };
